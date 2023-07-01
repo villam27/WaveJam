97c1dccec16561dcfa579aac8e8a68e67abf33b0
villam27 <villam2703@protonmail.com>
2023-07-01 15:07:01 +0200

    [ADD] animation when moving

--- src/player.rs
+++ src/player.rs
@@ -12,6 +12,12 @@ pub struct AData {
 pub struct Player;
 
 #[derive(Component)]
+pub struct PlayerBody;
+
+#[derive(Component)]
+pub struct PlayerHead;
+
+#[derive(Component)]
 pub struct FrameTime(f32);
 
 #[derive(Component)]
@@ -32,6 +38,7 @@ pub struct SpriteAnimationData {
  */
 #[derive(Bundle)]
 struct PlayerBodyBundle {
+	body: PlayerBody,
 	frame_time: FrameTime,
 	current_anim: CurrAnimId,
 	animation_data: SpriteAnimationData,
@@ -42,6 +49,7 @@ struct PlayerBodyBundle {
 
 #[derive(Bundle)]
 struct PlayerHeadBundle {
+	head: PlayerHead,
 	frame_time: FrameTime,
 	current_anim: CurrAnimId,
 	animation_data: SpriteAnimationData,
@@ -62,6 +70,8 @@ impl Plugin for PlayerPlugin {
 	fn build(&self, app: &mut App) {
 		app.add_startup_system(init_player)
 			.add_system(player_input)
+			.add_system(change_head)
+			.add_system(change_body)
 			.add_system(animate);
 	}
 }
@@ -76,9 +86,12 @@ fn init_player(mut command : Commands, asset_server: Res<AssetServer>, mut atlas
 	let head_texture_handle: Handle<TextureAtlas> = atlases.add(head_texture_atlas);
 	let mut head_animation_data: Vec<AData> = Vec::new();
 	
+	body_animation_data.push(AData {start: 0, end: 0, frame_time: 0.15});
 	body_animation_data.push(AData {start: 0, end: 3, frame_time: 0.15});
-	head_animation_data.push(AData {start: 0, end: 3, frame_time: 0.25});
-
+	head_animation_data.push(AData {start: 0, end: 0, frame_time: 0.25});
+	head_animation_data.push(AData {start: 1, end: 1, frame_time: 0.25});
+	head_animation_data.push(AData {start: 2, end: 2, frame_time: 0.25});
+	head_animation_data.push(AData {start: 3, end: 3, frame_time: 0.25});
 	command.spawn(PlayerBundle 
 	{
 		player: Player,
@@ -86,6 +99,7 @@ fn init_player(mut command : Commands, asset_server: Res<AssetServer>, mut atlas
 		speed: PlayerSpeed(Vec3::new(150., 150., 0.))
 	}).with_children(|parent: &mut ChildBuilder<'_, '_, '_>| {
 	parent.spawn(PlayerBodyBundle {
+		body: PlayerBody,
 		frame_time: FrameTime(0.0),
 		current_anim: CurrAnimId(0),
 		sprite: SpriteSheetBundle {
@@ -97,6 +111,7 @@ fn init_player(mut command : Commands, asset_server: Res<AssetServer>, mut atlas
 	});
 	}).with_children(|parent| {
 	parent.spawn(PlayerHeadBundle {
+		head: PlayerHead,
 		frame_time: FrameTime(0.0),
 		current_anim: CurrAnimId(0),
 		sprite: SpriteSheetBundle {
@@ -123,7 +138,10 @@ pub fn animate(mut query: Query<(&mut TextureAtlasSprite, &mut SpriteAnimationDa
 	}
 }
 
-pub fn player_input(keys: Res<Input<KeyCode>>, time: Res<Time>, mut query: Query<&mut Transform, With<Player>>, speed_query: Query<&mut PlayerSpeed, With<Player>>)
+pub fn player_input(keys: Res<Input<KeyCode>>, time: Res<Time>,
+		mut query: Query<&mut Transform, With<Player>>,
+		speed_query: Query<&mut PlayerSpeed, With<Player>>,
+	)
 {
 	let mut transform = query.single_mut();
 	let	speed = speed_query.single();
@@ -132,7 +150,7 @@ pub fn player_input(keys: Res<Input<KeyCode>>, time: Res<Time>, mut query: Query
 	{
 		transform.translation.x += speed.0.x * time.delta_seconds();
 	}
-	if keys.pressed(KeyCode::Left)
+	else if keys.pressed(KeyCode::Left)
 	{
 		transform.translation.x -= speed.0.x * time.delta_seconds();
 	}
@@ -140,8 +158,47 @@ pub fn player_input(keys: Res<Input<KeyCode>>, time: Res<Time>, mut query: Query
 	{
 		transform.translation.y += speed.0.x * time.delta_seconds();
 	}
-	if keys.pressed(KeyCode::Down)
+	else if keys.pressed(KeyCode::Down)
 	{
 		transform.translation.y -= speed.0.x * time.delta_seconds();
+	}
+}
+
+pub fn change_head(keys: Res<Input<KeyCode>>,
+	mut head: Query<&mut CurrAnimId, With<PlayerHead>>,
+	mut tex: Query<(&mut TextureAtlasSprite, &SpriteAnimationData), With<PlayerHead>>
+)
+{
+	let mut head_id = head.single_mut();
+	let (mut texture, data) = tex.single_mut();
+
+	if keys.just_pressed(KeyCode::Right){
+		head_id.0 = 1;
+	}
+	if keys.just_pressed(KeyCode::Left){
+		head_id.0 = 2;
+	}
+	if keys.just_pressed(KeyCode::Up){
+		head_id.0 = 3;
+	}
+	if keys.just_pressed(KeyCode::Down){
+		head_id.0 = 0;
+	}
+	texture.index = data.data[head_id.0].start;
+}
+
+pub fn change_body(keys: Res<Input<KeyCode>>,
+	mut body: Query<&mut CurrAnimId, With<PlayerBody>>,
+	mut tex: Query<(&mut TextureAtlasSprite, &SpriteAnimationData), With<PlayerBody>>
+)
+{
+	let mut body_id = body.single_mut();
+	let (mut texture, data) = tex.single_mut();
+
+	if keys.any_pressed([KeyCode::Right, KeyCode::Left, KeyCode::Up, KeyCode::Down]) {
+		body_id.0 = 1;
+	}
+	else {
+		body_id.0 = 0;
 	}
 }
