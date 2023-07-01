5244ebe2542d501173e658fdc28f64bf9eafbd24
villam27 <villam2703@protonmail.com>
2023-07-01 11:36:36 +0200

    [ADD] game plugin, MainCamera and main Main PluginGroup

---
+++ src/game.rs
@@ -0,0 +1,26 @@
+use bevy::prelude::*;
+
+const BACKGROUND_COLOR: Color = Color::rgb(0.055, 0.957, 0.988);
+pub struct GamePlugin;
+
+#[derive(Component)]
+struct MainCamera;
+
+fn start_game(mut command : Commands, asset_server: Res<AssetServer>) {
+	command.spawn((Camera2dBundle {
+									transform: Transform::from_xyz(142.0, 0.0, 0.0),
+									..default()
+								}, MainCamera));
+	command.spawn(SpriteBundle {
+									transform: Transform::from_xyz(56.0, 242.0, 0.0),
+									texture: asset_server.load("player/head.png"),
+									..default()
+								});
+}
+
+impl Plugin for GamePlugin {
+	fn build(&self, app: &mut App) {
+		app.insert_resource(ClearColor(BACKGROUND_COLOR))
+			.add_startup_system(start_game);
+	}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, app::PluginGroupBuilder};
+use game::GamePlugin;
 use player::PlayerPlugin;
+
 mod player;
 mod game;
+
+struct MainPlugin;
+
+impl PluginGroup for MainPlugin {
+	fn build(self) -> bevy::app::PluginGroupBuilder {
+		PluginGroupBuilder::start::<Self>()
+			.add(GamePlugin)
+			.add(PlayerPlugin)
+	}
+}
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(PlayerPlugin)
+        .add_plugins(MainPlugin)
         .run();
 }
