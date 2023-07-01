96c3aaf4f5630197e27bd7ca90a06a73cd19c104
Nestyles <tom.sancho@epitech.eu>
2023-07-01 15:46:30 +0200

    [ADD] Default map loading with ldtk

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,38 @@
 use bevy::prelude::*;
+use bevy_ecs_ldtk::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(
+            DefaultPlugins.set(ImagePlugin::default_nearest()), // prevents blurry sprites
+        )
+        .add_plugin(LdtkPlugin)
+        .add_startup_system(setup)
+        .insert_resource(LevelSelection::Index(0))
+        .register_ldtk_entity::<MyBundle>("MyEntityIdentifier")
         .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn(LdtkWorldBundle {
+        ldtk_handle: asset_server.load("my_project.ldtk"),
+        ..Default::default()
+    });
+}
+
+#[derive(Default, Component)]
+struct ComponentA;
+
+#[derive(Default, Component)]
+struct ComponentB;
+
+#[derive(Bundle, LdtkEntity)]
+pub struct MyBundle {
+    a: ComponentA,
+    b: ComponentB,
+    #[sprite_sheet_bundle]
+    #[bundle]
+    sprite_bundle: SpriteSheetBundle,
 }
